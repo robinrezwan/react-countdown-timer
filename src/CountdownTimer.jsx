14afe3563c7877f9db5c6a213d3f2bcd6233d187
Robin Rezwan <rezwankabirrobin@gmail.com>
2023-03-07 10:35:02 +0600

    Extracted countdown to separate component

---
+++ src/CountdownTimer.jsx
@@ -0,0 +1,75 @@
+import {useEffect, useState} from 'react'
+import CircularProgress from "./CircularProgress.jsx";
+
+function CountdownTimer({dateSince, dateUntil}) {
+    const [totalTime, setTotalTime] = useState(null);
+    const [remainingTime, setRemainingTime] = useState(null);
+
+    useEffect(() => {
+        const dateNow = new Date();
+
+        const totalDuration = getDuration(dateSince, dateUntil);
+        setTotalTime(totalDuration)
+        console.log("totalDuration", totalDuration);
+
+        const remainingDuration = getDuration(dateNow, dateUntil);
+        setRemainingTime(remainingDuration);
+        console.log("remainingDuration", remainingDuration);
+
+        updateDuration();
+    }, []);
+
+    function getDuration(dateSince, dateUntil) {
+        // Get the time difference in milliseconds
+        const timeDiff = Math.abs(dateUntil.getTime() - dateSince.getTime());
+
+        // Convert the time difference to days, hours, minutes, and seconds
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+        // console.log(`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+
+        return {days, hours, minutes, seconds};
+    }
+
+    function updateDuration() {
+        const dateNow = new Date();
+        const duration = getDuration(dateNow, dateUntil);
+        setRemainingTime(duration);
+        setTimeout(updateDuration, 200);
+    }
+
+    return (<div>
+        {(remainingTime && totalTime) ?
+            <div style={{display: "flex", gap: "20px", padding: "10px"}}>
+                <CircularProgress
+                    value={remainingTime.days}
+                    maxValue={totalTime.days}
+                    text={remainingTime.days}
+                    footerText="Days"
+                />
+                <CircularProgress
+                    value={remainingTime.hours}
+                    maxValue={60}
+                    text={remainingTime.hours}
+                    footerText="Hours"
+                />
+                <CircularProgress
+                    value={remainingTime.minutes}
+                    maxValue={60}
+                    text={remainingTime.minutes}
+                    footerText="Minutes"
+                />
+                <CircularProgress
+                    value={remainingTime.seconds}
+                    maxValue={60}
+                    text={remainingTime.seconds}
+                    footerText="Seconds"
+                />
+            </div> : null}
+    </div>);
+}
+
+export default CountdownTimer;
